511996d15a79890b2cf6460ee044abae5cf9a412
Amanda Pettenati <amanda@pette.net>
2021-09-01 23:23:55 -0400

    Add config type to eslint

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,23 +1,25 @@
-const configuration = {
+import { Linter } from "eslint";
+
+const configuration: Linter.BaseConfig = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   rules: {
-    'max-len': ['warn', { code: 80 }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  }
-}
+    "max-len": ["warn", { code: 80 }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
 
-export default configuration
+export default configuration;
