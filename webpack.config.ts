03079e72f44f07184088034d78bf78f1cb902cee
Amanda Pettenati <amanda@pette.net>
2021-09-01 22:43:46 -0400

    Add hot reload

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,36 @@
-import * as path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import { Configuration } from 'webpack'
+import * as path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-const configuration: Configuration = {
-  entry: {
-    bundle: path.join(path.resolve(), 'src', 'index.tsx')
-  },
-  target: 'web',
+const configuration = {
+  entry: { bundle: path.join(path.resolve(), "src", "index.tsx") },
+  target: "web",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        enforce: 'pre',
-        use: ['ts-loader', 'source-map-loader'],
-        exclude: /node_modules/
-      }
-    ]
+        enforce: "pre",
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "/./dist",
+    hot: true,
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(path.resolve(), './dist')
+    filename: "bundle.js",
+    path: path.resolve(path.resolve(), "./dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(path.resolve(), './public', 'index.html')
-    })
-  ]
-}
+      template: path.join(path.resolve(), "./public", "index.html"),
+    }),
+  ],
+};
 
-export default configuration
+export default configuration;
