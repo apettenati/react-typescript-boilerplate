0d314d5200362bc7b73e2bd15fa95eeaa6ddb646
Amanda Pettenati <amanda@pette.net>
2021-10-14 13:10:05 -0400

    Fix formatting with eslint

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,44 +1,45 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { Configuration, WebpackOptionsNormalized } from "webpack";
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { Configuration, WebpackOptionsNormalized } from 'webpack'
 
 /**
  * WebpackOptionsNormalized included to extend config type to include devServer
  * SEE: https://github.com/webpack/webpack/issues/13621
  * */
 const configuration: Configuration | WebpackOptionsNormalized = {
+  mode: 'development',
   entry: {
-    bundle: path.resolve(__dirname, "src", "index.tsx"),
+    bundle: path.resolve(__dirname, 'src', 'index.tsx'),
   },
-  target: "web",
+  target: 'web',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        enforce: "pre",
-        use: ["ts-loader"],
+        // enforce: "pre", // why is this necessary?
+        use: ['ts-loader'],
         exclude: /node_modules/,
       },
     ],
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: "/./dist",
+    static: '/./dist',
     hot: true,
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
       clean: true,
     }),
   ],
-};
+}
 
-export default configuration;
+export default configuration
